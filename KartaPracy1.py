12141f5a2971ec46f1164154771f272f9bd8f5fa
1b.31.PW <114579298+P13TR453Q@users.noreply.github.com>
2022-10-03 11:19:34 +0000

    Zrobiono zad. 1-4 z Karty Pracy

---
+++ KartaPracy1.py
@@ -0,0 +1,18 @@
+#Zad 1
+# a = int(input())
+# b = int(input())
+# print(a**2 + b**2)
+
+# #Zad 2
+# a = int(input())
+# b = int(input())
+# print((a + b)**2)
+#Zad 4
+# a = int(input())
+# b = int(input())
+# print((a - b) ** 3 )
+#Zad 4 
+# a = int(input())
+# b = int(input())
+# c = int(input())
+# print( a * b * c)

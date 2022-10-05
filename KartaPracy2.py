fae7170f339ac67dd135dbb1b3a9814fe384b8a6
1b.31.PW <114579298+P13TR453Q@users.noreply.github.com>
2022-10-05 11:17:33 +0000

    Zrobiono  zad 1-3 z KartaPracy2.pdf

---
+++ KartaPracy2.py
@@ -0,0 +1,20 @@
+#Zad 1
+# a = int(input("liczba"))
+# if a % 3 == 0:
+#   print("yes")
+# else:
+#   print("no")
+#Zad 2
+# a = int(input())
+# if a > 100 and a < 1000 and a % 17 == 0:
+#   print("yes")
+# else:
+#   print("no")
+
+#Zad 3
+
+# wiek = int(input("podaj wiek: "))
+# if wiek >= 18:
+#   print("yes")
+# else:
+#   print("no")
